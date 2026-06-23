3d8932844a150535476be02a5430fa3251caad57
arrrtsiomm <artemijzagzeckij@gmail.com>
2026-06-23 10:22:37 +0300

    25% of 'intro to str' completed

---
+++ introtostrings.py
@@ -0,0 +1,14 @@
+my_str = 'Hello world'
+
+print('Hello' in my_str) #True
+print('Hell' in my_str) #True
+print('hey' in my_str) #False
+print('f' in my_str)
+
+print(len(my_str))
+
+print(my_str[0]) #H
+print(my_str[6]) #w
+print(my_str[-1]) #d
+print(my_str[-2]) #l
+
